56bbaf0bb1855941c354d52040a2609f5f078126
Pralav Dessai <pralavgoa@gmail.com>
2014-04-18 14:25:19 -0700

    added script to start wise automatically

---
+++ latest binaries/run scripts/runWise.bat
@@ -0,0 +1,15 @@
+@echo off
+rem -------------------------------------------------------------------------
+rem Script to run working version of WISE
+rem -------------------------------------------------------------------------
+
+rem WISE_HOME should be pointed at the directory containing the properties file for WISE
+set WISE_HOME=C:\_Pralav\workspace\SecureWise\configuration\wise\dev\
+
+set WISE_DIST=C:\_Pralav\workspace\SecureWise\latest binaries
+set TOMCAT_HOME=C:\_Pralav\tools\apache-tomcat-7.0.47
+
+copy "%WISE_DIST%\WISE.war" "%TOMCAT_HOME%\webapps"
+
+cd %TOMCAT_HOME%/bin
+startup.bat
